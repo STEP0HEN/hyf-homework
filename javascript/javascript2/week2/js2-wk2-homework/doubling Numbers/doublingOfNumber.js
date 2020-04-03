ca548be6eb46026228cca295dd726f6d95cb04ca
STEP0HEN <57806191+STEP0HEN@users.noreply.github.com>
2020-04-03 23:42:12 +0200

    Revert "added js2 week2 homework"

--- javascript/javascript2/week2/js2-wk2-homework/doubling Numbers/doublingOfNumber.js
+++
@@ -1,6 +0,0 @@
-"use strict"
-
-let numbers = [1, 2, 3, 4];
-const newNumbers = numbers.filter(OddNumbers => OddNumbers % 2 !== 0).map(multiplyOddNumbers => multiplyOddNumbers * 2);
-console.log(newNumbers)
-
