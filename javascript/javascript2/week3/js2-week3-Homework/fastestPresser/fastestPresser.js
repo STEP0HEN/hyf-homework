0280a905b5ac2f75e368dac894cfe810ccdd775b
STEP0HEN <guchy17@gmail.com>
2020-04-15 00:16:16 +0200

    made changes to js2wk3 fastPresser

--- javascript/javascript2/week3/js2-week3-Homework/fastestPresser/fastestPresser.js
+++ javascript/javascript2/week3/js2-week3-Homework/fastestPresser/fastestPresser.js
@@ -6,6 +6,10 @@ const playerS = document.querySelector('.player-S')
 const playerL = document.querySelector('.player-L')
 const result = document.querySelector('.result')
 const restart = document.querySelector('.restart')
+const newUlTag = document.createElement('ul')
+const ulTag = document.createElement('ul')
+playerS.appendChild(newUlTag)
+playerL.appendChild(ulTag)
 
 let counterL = 0;
 let counterS = 0;
@@ -15,14 +19,15 @@ function beginGame () {
     setTimeout(()=>{
 if(!timeInput.value){
     alert('please enter time to play game')
+    !gameResult
 }else alert('Game over')
 
 const gameResult = () => {
     if(counterS > counterL) {
-        result.innerHTML = `Player S is the fastest typer on the planet. Congrats!!`
+        result.innerHTML = `Player S won the game. Congrats!!`
     }
     if (counterL > counterS) {
-        result.innerHTML = `Player L is the fastest typer on the planet. Congrats!!`
+        result.innerHTML = `Player L won the game. Congrats!!`
     }
     if (counterS === counterL) {
         result.innerHTML = `It was a tie.`
@@ -40,13 +45,19 @@ gameStart.addEventListener('click', beginGame)
 function keyPressCount (keyboardPress) {
     if(keyboardPress.key === 'l') {
         counterL++
-        playerS.innerHTML = `${counterL}`
+        ulTag.innerHTML = `${counterL}`
         console.log(`${counterL}`)
     }
     if(keyboardPress.key === 's') {
         counterS++
-        playerS.innerHTML = `${counterS}`
+        newUlTag.innerHTML = `${counterS}`
         console.log(`${counterS}`)
+    }
+     if (!timeInput.value){
+        ulTag.innerHTML = null
+        newUlTag.innerHTML = null
+        counterS = null
+        counterL = null
     }
 }
 
@@ -57,4 +68,4 @@ document.addEventListener('keypress',keyPressCount)
 function startOver () {
     location.reload()
 }
-restart.addEventListener('click',startOver)
+restart.addEventListener('click',startOver);
