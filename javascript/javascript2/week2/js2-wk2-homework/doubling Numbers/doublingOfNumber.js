6689083938248d0002b320702fd3d29e77134200
STEP0HEN <guchy17@gmail.com>
2020-05-08 23:21:46 +0200

    implemented changes

--- javascript/javascript2/week2/js2-wk2-homework/doubling Numbers/doublingOfNumber.js
+++ javascript/javascript2/week2/js2-wk2-homework/doubling Numbers/doublingOfNumber.js
@@ -1,6 +1,6 @@
 "use strict"
 
 let numbers = [1, 2, 3, 4];
-const newNumbers = numbers.filter(OddNumbers => OddNumbers % 2 !== 0).map(multiplyOddNumbers => multiplyOddNumbers * 2);
+const newNumbers = numbers.filter(num => num % 2 !== 0).map(Num => Num * 2);
 console.log(newNumbers)
 
