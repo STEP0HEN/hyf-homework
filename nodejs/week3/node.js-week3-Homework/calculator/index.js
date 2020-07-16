d268d2ccbb808de9845983d6cf71a5ffdbf92427
STEP0HEN <guchy17@gmail.com>
2020-07-16 17:24:30 +0200

    implemented changes

--- nodejs/week3/node.js-week3-Homework/calculator/index.js
+++ nodejs/week3/node.js-week3-Homework/calculator/index.js
@@ -4,60 +4,51 @@ const port = 3000;
 calculator.use(express.json());
 calculator.use(express.urlencoded({ extended: true }));
 
+function calculatorFormular(method, arrayOfNumbers) {
+	if (method === "add") {
+		const total = arrayOfNumbers.reduce((total, num) => total + num);
+		return total;
+	}
+
+	if (method === "multiply") {
+		const product = arrayOfNumbers.reduce((total, num) => total * num);
+		return product;
+	}
+
+	if (method === "subtract") {
+		const difference = arrayOfNumbers.reduce((total, num) => total - num);
+		return difference;
+	}
+
+	if (method === "divide") {
+		const quotient = arrayOfNumbers.reduce((total, num) => total / num);
+		return quotient;
+	}
+}
+
 // Calculator using :method (path) parameter:
 calculator.get("/calculator/:method", (request, response) => {
+	const method = request.params.method;
 	const queryParams = Object.values(request.query);
 	const numsMerged = [].concat.apply([], queryParams);
 	const calculateQueryParams = numsMerged.map(Number);
 
-	if (request.params.method === "multiply") {
-		const product = calculateQueryParams.reduce((total, num) => total * num);
-		response.send(`product = ${product}`);
-	}
-
-	if (request.params.method === "subtract") {
-		const difference = calculateQueryParams.reduce((total, num) => total - num);
-		response.send(`${difference}`);
-	}
-
-	if (request.params.method === "add") {
-		const total = calculateQueryParams.reduce((total, num) => total + num);
-		response.send(`total = ${total}`);
-	}
-
-	if (request.params.method === "divide") {
-		const quotient = calculateQueryParams.reduce((total, num) => total / num);
-		response.send(`Answer = ${quotient}`);
-	}
+	const answer = calculatorFormular(method, calculateQueryParams);
+	response.json(answer);
 });
 
 //Calculate using request.body
 
 calculator.post("/calculator", (request, response) => {
 	const method = request.body.method;
-	const queryParams = Object.values(request.query);
-	const numsMerged = [].concat.apply([], queryParams);
+	const firstParam = Object.values(request.body.firstParam);
+	const secondParam = Object.values(request.body.secondParam);
+	const numsMerged = [].concat.apply([firstParam], secondParam);
 	const calculateQueryParams = numsMerged.map(Number);
 
-	if (method === "add") {
-		const total = calculateQueryParams.reduce((total, num) => total + num);
-		response.send(`answer = ${total}`);
-	}
+	const result = calculatorFormular(method, calculateQueryParams);
 
-	if (method === "multiply") {
-		const product = calculateQueryParams.reduce((total, num) => total * num);
-		response.send(`product = ${product}`);
-	}
-
-	if (method === "subtract") {
-		const difference = calculateQueryParams.reduce((total, num) => total - num);
-		response.send(`difference = ${difference}`);
-	}
-
-	if (method === "divide") {
-		const quotient = calculateQueryParams.reduce((total, num) => total / num);
-		response.send(`answer = ${quotient}`);
-	}
+	response.json(result);
 });
 
 calculator.listen(port, () => {
