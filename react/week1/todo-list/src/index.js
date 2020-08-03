542e7a37abdd663fa99e2955ff92d377a67331c7
STEP0HEN <guchy17@gmail.com>
2020-08-03 04:32:18 +0200

    implemented changes

--- react/week1/todo-list/src/index.js
+++ react/week1/todo-list/src/index.js
@@ -1,45 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {Todo} from "./Todo";
+import {Title }from "./Title";
 import "./index.css";
 
-const todo = [
-	{
-		description: " Get out of bed",
-		deadline: " Wed Sep 13 2017",
-	},
-	{
-		description: "Brush teeth",
-		deadline: " Thu Sep 14 2017",
-	},
-
-	{
-		description: "Eat breakfast",
-		deadline: "Fri Sep 15 2017 ",
-	},
-];
-
-class Title extends React.Component {
-	render() {
-		return <h1>Todo List</h1>;
-	}
-}
-
-const Todo = (props) => {
-	const list = props.list;
-	const myToDoList = list.map((tasks) => {
-		return (
-			<li>
-				{tasks.description} , {tasks.deadline}
-			</li>
-		);
-	});
-	return <ul>{myToDoList}</ul>;
-};
 
 ReactDOM.render(
 	<div className="container">
 		<Title />
-		<Todo list={todo} />
+		<Todo />
 	</div>,
 	document.getElementById("root")
 );
